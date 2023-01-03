cfb4472c32457b92066239b316ee71f0858817f2
goutarouh <gouta.gorogoro@gmail.com>
2023-01-03 12:06:51 +0900

    featureAとfeatureBにComposeの依存を追加

---
+++ feature/featurea/src/main/java/com/github/goutarouh/feature/featurea/FeatureAScreen.kt
@@ -0,0 +1,7 @@
+package com.github.goutarouh.feature.featurea
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FeatureAScreen() {
+}
---
+++ feature/featureb/src/main/java/com/github/goutarouh/feature/featureb/FeatureBScreen.kt
@@ -0,0 +1,8 @@
+package com.github.goutarouh.feature.featureb
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FeatureBScreen() {
+
+}
