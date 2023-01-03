98415789df2aa9c5e8c8e606519f8f0d8a3e0543
goutarouh <gouta.gorogoro@gmail.com>
2023-01-03 17:48:45 +0900

    stringとアプリケーション名の修正

--- app/src/main/java/com/github/goutarouh/androidsampleapp/AndroidSamplAppApplication.kt
+++ app/src/main/java/com/github/goutarouh/androidsampleapp/AndroidSamplAppApplication.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HiltSampleApplication: Application() {
+class AndroidSampleAppApplication: Application() {
 }
