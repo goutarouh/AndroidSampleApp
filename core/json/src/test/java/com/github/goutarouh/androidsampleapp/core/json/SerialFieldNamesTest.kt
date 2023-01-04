50d0e7b1ae0f32e6f3c477e46d0f0572abf02e19
goutarouh <gouta.gorogoro@gmail.com>
2023-01-04 16:48:41 +0900

    json関連のテスト追加

---
+++ core/json/src/test/java/com/github/goutarouh/androidsampleapp/core/json/SerialFieldNamesTest.kt
@@ -0,0 +1,36 @@
+package com.github.goutarouh.androidsampleapp.core.json
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.Assert
+import org.junit.Test
+
+class SerialFieldNamesTest {
+
+    @Test
+    fun encode_isCorrect() {
+        val actual = Json.encodeToString(decodeData)
+        Assert.assertEquals(encodedData, actual)
+    }
+
+    @Test
+    fun decode_isCorrect() {
+        val actual = Json.decodeFromString<SFNProject>(encodedData)
+        Assert.assertEquals(decodeData, actual)
+    }
+
+
+}
+
+@Serializable
+private data class SFNProject(
+    @SerialName("is_going")
+    val isGoing: Boolean,
+    @SerialName("lang")
+    val language: String
+)
+private val decodeData = SFNProject(true, "jn")
+private const val encodedData = """{"is_going":true,"lang":"jn"}"""
