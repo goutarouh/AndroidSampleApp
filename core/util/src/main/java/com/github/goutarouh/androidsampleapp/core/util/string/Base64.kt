e6adeec7028717f7673bc875f28846a19e490e69
goutarouh <gouta.gorogoro@gmail.com>
2023-01-14 16:07:29 +0900

    #13 base64の変換処理をutilとして追加

---
+++ core/util/src/main/java/com/github/goutarouh/androidsampleapp/core/util/string/Base64.kt
@@ -0,0 +1,11 @@
+package com.github.goutarouh.androidsampleapp.core.util.string
+
+import java.util.Base64
+
+fun String.encode64(): String {
+    return Base64.getEncoder().encodeToString(this.toByteArray())
+}
+
+fun String.decode64(): String {
+    return String(Base64.getDecoder().decode(this))
+}
