3b194b48398eb501a0bf6eda3ad0019270d7615f
goutarouh <gouta.gorogoro@gmail.com>
2023-01-20 19:47:18 +0900

    #34 baseモジュールのbuild.gradle修正

--- core/base/build.gradle.kts
+++ core/base/build.gradle.kts
@@ -1,16 +1,12 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("androidsampleapp.android.library")
+    id("androidsampleapp.android.hilt")
 }
 
 android {
-    namespace = "com.example.base"
-    compileSdk = 33
+    namespace = "com.github.goutarouh.androidsampleapp.core.base"
 
     defaultConfig {
-        minSdk = 26
-        targetSdk = 33
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -24,17 +20,9 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
