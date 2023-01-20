924f08085767566d04c61a8ac2405db33ecfe406
goutarouh <gouta.gorogoro@gmail.com>
2023-01-20 12:27:15 +0900

    #34 featurebとfeaturebへの依存などを削除

--- app/src/main/java/com/github/goutarouh/androidsampleapp/navigation/MainNavigation.kt
+++ app/src/main/java/com/github/goutarouh/androidsampleapp/navigation/MainNavigation.kt
@@ -6,8 +6,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.github.goutarouh.androidsampleapp.feature.rss.rsshome.RSS_HOME_ROUTE
 import com.github.goutarouh.androidsampleapp.feature.rss.rsshome.rssHome
-import com.github.goutarouh.androidsampleapp.feature.featureb.featureBScreen
-import com.github.goutarouh.androidsampleapp.feature.featurec.featureCScreen
 
 @Composable
 fun MainNavigation(
@@ -16,9 +14,5 @@ fun MainNavigation(
 ) {
     NavHost(navController = navController, startDestination = RSS_HOME_ROUTE) {
         rssHome(navController, modifier)
-        featureBScreen() {
-            navController.navigate(RSS_HOME_ROUTE)
-        }
-        featureCScreen()
     }
 }
