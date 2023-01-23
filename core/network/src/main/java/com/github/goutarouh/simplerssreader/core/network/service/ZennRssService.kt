0a703bdc1b63fcf7a993f51c8ed4919003acbf29
goutarouh <gouta.gorogoro@gmail.com>
2023-01-23 20:44:32 +0900

    #44 不要なimportを削除

--- core/network/src/main/java/com/github/goutarouh/simplerssreader/core/network/service/ZennRssService.kt
+++ core/network/src/main/java/com/github/goutarouh/simplerssreader/core/network/service/ZennRssService.kt
@@ -2,8 +2,6 @@ package com.github.goutarouh.simplerssreader.core.network.service
 
 import com.github.goutarouh.simplerssreader.core.network.data.rss.RssApiModel
 import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface ZennRssService {
