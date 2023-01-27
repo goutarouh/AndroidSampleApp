32d5f97d87c1a3da02d7dbe72370a4021196631e
goutarouh <gouta.gorogoro@gmail.com>
2023-01-27 18:47:43 +0900

    #63 通知用の設定rowを追加

---
+++ feature/rss/src/main/java/com/github/goutarouh/simplerssreader/feature/rss/rssitemlist/settings/RssItemSettingAutoUpdateRow.kt
@@ -0,0 +1,70 @@
+package com.github.goutarouh.simplerssreader.feature.rss.rssitemlist.settings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.goutarouh.simplerssreader.core.ui.annotation.DayAndNightPreviews
+import com.github.goutarouh.simplerssreader.core.ui.theme.SrrTheme
+import com.github.goutarouh.simplerssreader.feature.rss.R
+
+@Composable
+fun RssItemSettingAutoUpdateRow(
+    modifier: Modifier = Modifier
+) {
+    var checked by remember { mutableStateOf(false) }
+    Column {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.auto_update),
+                contentDescription = null
+            )
+            Text(
+                text = stringResource(id = R.string.rss_settings_auto_update),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp),
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.h4
+            )
+            Switch(
+                checked = checked,
+                onCheckedChange = { checked = it }
+            )
+        }
+        Text(
+            text = stringResource(id = R.string.rss_settings_auto_update_description),
+            fontSize = 13.sp,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            style = MaterialTheme.typography.caption,
+        )
+    }
+}
+
+@DayAndNightPreviews
+@Composable
+fun PreviewRssItemSettingAutoUpdateRow() {
+    SrrTheme {
+        Surface {
+            RssItemSettingAutoUpdateRow()
+        }
+    }
+}
