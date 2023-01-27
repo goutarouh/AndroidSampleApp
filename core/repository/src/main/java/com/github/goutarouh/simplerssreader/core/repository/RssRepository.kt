a2b73e709571beca21ec551c5c9211f71787202d
goutarouh <gouta.gorogoro@gmail.com>
2023-01-27 19:49:39 +0900

    #63 repositoryにpushを設定する処理を追加

--- core/repository/src/main/java/com/github/goutarouh/simplerssreader/core/repository/RssRepository.kt
+++ core/repository/src/main/java/com/github/goutarouh/simplerssreader/core/repository/RssRepository.kt
@@ -32,6 +32,7 @@ interface RssRepository {
     suspend fun updateRss(rssLink: String, isInit: Boolean): Result<Rss>
     suspend fun getRss(rssLink: String): Result<Rss>
     suspend fun setAutoFetch(rssLink: String, isAutoFetch: Boolean)
+    suspend fun setPushNotification(rssLink: String, isPushNotification: Boolean)
     suspend fun setUnReadItemCount(rssLink: String, count: Int)
     suspend fun updateRssAndCheckNewItemCount(rssLink: String): Int
     suspend fun deleteAndUnregisterRss(rssLink: String)
@@ -85,7 +86,11 @@ internal class RssRepositoryImpl(
     }
 
     override suspend fun setAutoFetch(rssLink: String, isAutoFetch: Boolean) = withContext(Dispatchers.IO) {
-        rssDao.updateRssMetaEntity(rssLink, isAutoFetch)
+        rssDao.updateRssMetaEntityWithAutoFetch(rssLink, isAutoFetch)
+    }
+
+    override suspend fun setPushNotification(rssLink: String, isPushNotification: Boolean) {
+        rssDao.updateRssMetaEntityWithPushNotification(rssLink, isPushNotification)
     }
 
     override suspend fun setUnReadItemCount(rssLink: String, count: Int) {
