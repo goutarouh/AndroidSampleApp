99c4150d88feeb2b022311342086ec090acfccf5
goutarouh <gouta.gorogoro@gmail.com>
2023-02-08 20:28:19 +0900

    #67 multipreviewに説明を追加

--- core/ui/src/main/java/com/github/goutarouh/simplerssreader/core/ui/annotation/MultiFontScalePreviews.kt
+++ core/ui/src/main/java/com/github/goutarouh/simplerssreader/core/ui/annotation/MultiFontScalePreviews.kt
@@ -3,9 +3,13 @@ package com.github.goutarouh.simplerssreader.core.ui.annotation
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
+    name = "0.75 scale",
+    group = "font scale",
     fontScale = 0.75f
 )
 @Preview(
+    name = "1.5 scale",
+    group = "font scale",
     fontScale = 1.5f
 )
 annotation class MultiFontScalePreviews()
